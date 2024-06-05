d2181807de66abea74b4a1824a1e718a86875b68
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-05 13:09:12 +0700

    Create getHumanChoice func

--- scripts.js
+++ scripts.js
@@ -1,16 +1,37 @@
 function getComputerChoice() {
-    let randNum =  Math.random();
-    if (randNum < 0.33) {
-        return "rock";
-    } else if (randNum >= 0.33 && randNum < 0.66) {
-        return "paper";
+  let randNum = Math.random();
+  if (randNum < 0.33) {
+    return "rock";
+  } else if (randNum >= 0.33 && randNum < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let userPrint;
+  while (true) {
+    userPrint = prompt("Choice rock, paper or scissors: ");
+
+    if (userPrint === null) break;
+
+    userPrint = userPrint.toLowerCase();
+
+    if (
+      userPrint === "rock" ||
+      userPrint === "paper" ||
+      userPrint === "scissors"
+    ) {
+      return userPrint;
     } else {
-        return "scissors";
+      alert("Incorrect value, please try again!");
     }
+  }
 }
 
 function main() {
-    console.log(getComputerChoice());
+  console.log(getHumanChoice());
 }
 
 main();
