9996573e9ba00c6456fff970f2fccbecc7e6999d
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-05 12:57:57 +0700

    Create getComputerChoice func

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("test");
+function getComputerChoice() {
+    let randNum =  Math.random();
+    if (randNum < 0.33) {
+        return "rock";
+    } else if (randNum >= 0.33 && randNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function main() {
+    console.log(getComputerChoice());
+}
+
+main();
